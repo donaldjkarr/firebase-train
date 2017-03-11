ed79cec1d08bbd73a79539af17d37f08242bcd55
donaldjkarr <donaldjkarr@gmail.com>
2017-03-10 21:19:23 -0600

    Second update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
 
   var trainName = $("#train-name-input").val().trim();
   var destination = $("#destination-input").val().trim();
-  var first = moment($("#first-train-input").val().trim(), "DD/MM/YY").format("X");
+  var first = moment($("#first-train-input").val().trim(), "HH:mm").format("X");
   var frequency = $("#frequency-input").val().trim();
 
   var newTrain = {
@@ -48,7 +48,39 @@
 //pull from firebase and add html to table
   database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
+  	console.log(childSnapshot.val());
+
+  	var trainName = childSnapshot.val().name;
+  	var destination = childSnapshot.val().trainDestination;
+  	var first = childSnapshot.val().firstTrain;
+  	var frequency = childSnapshot.val().trainFrequency;
 
 
+
+  	console.log(trainName);
+  	console.log(destination);
+  	console.log(first);
+  	console.log(frequency);
+
+  	var currentTime = moment();
+  	console.log("current time: " + moment(currentTime).format("HH:mm"));
+
+  	var firstTimeConverted = moment(first, "X").subtract(1, "years");
+  	console.log(firstTimeConverted);
+
+  	var trainArrival = moment().diff(moment.unix(first, "X"), "minutes");
+  	console.log(trainArrival);
+
+  	var remainder = trainArrival % frequency;
+  	console.log(remainder);
+
+  	var untilTrain = frequency - remainder;
+  	console.log("minutes till train: " + untilTrain);
+
+  	var nextTrain = moment().add(untilTrain, "minutes").format("HH:mm");
+  	console.log("arrival time: " + moment(nextTrain));
+
+  	$("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextTrain + 
+  		"</td><td>" + untilTrain + "</td></tr>");
   });
 
